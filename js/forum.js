af470fc29bf6fb9f3cca8df6bf66df9061800f80
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2022-02-20 16:43:39 +0530

    newJS V.0.0

--- js/forum.js
+++
@@ -1,7 +0,0 @@
-function dropdiscussions(cat_id){
-    if(("#cat_disscussion_1"+cat_id).css("display")=="block"){
-        ("#cat_disscussion_1"+cat_id).css("display","none");
-    }else{
-        (`#cat_disscussion_1${cat_id}`).css("display","block");
-    }
-}
