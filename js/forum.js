f039f2d93cdc1f9189024ab7e99ea4ecf4d6cf87
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2022-02-24 00:49:32 +0530

    Setting up local storage

--- js/forum.js
+++ js/forum.js
@@ -44,3 +44,13 @@
     }
 
   ]
+
+  var threads=defaultThreads
+  if (localStorage && localStorage.getItem('threads')){
+      threads = JSON.parse(localStorage.getItem('threads'));
+
+  }
+  else{
+      threads=defaultThreads;
+      localStorage.setItem('threads', JSON.stringify(defaultThreads));
+  }
