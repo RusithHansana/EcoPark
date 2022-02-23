07e80d80bdcbb4919dee899fb1840f0fefc0cc79
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2022-02-23 23:37:42 +0530

    Added minor js

--- js/forum.js
+++ js/forum.js
@@ -1,63 +1,44 @@
-//Hiding function
-
-function showComments() {
-    var commentArea = document.getElementById("comments");
-    if (commentArea.style.display === "none") {
-        commentArea.style.display = "block";
-    } else {
-        commentArea.style.display = "none";
-    }
-  }
-
-function showReplies() {
-    var replyArea = document.getElementById("replies");
-    if (replyArea.style.display === "none") {
-        replyArea.style.display = "block";
-    } else {
-        replyArea.style.display = "none";
-    }
-  }
 
   var threads=[
       {
           id:1,
           title:"Thread 1",
-          author:"Aaron",
+          author:"EcoAuthor",
           date:Date.now(),
-          content:"Thread Contend",
+          content:"Thread Content 1",
           comments:
           [
               {
                   author:"EcoUser",
                   date:Date.now(),
-                  content:"Hey there"
+                  content:"Eco park...."
 
               },
               {
                   author:"EcoUser",
                   date:Date.now(),
-                  content:"Hey There"
+                  content:"Eco park....."
               }
           ]
       },
       {
         id:2,
         title:"Thread 2",
-        author:"Aaron",
+        author:"EcoAuthor",
         date:Date.now(),
-        content:"Thread Contend",
+        content:"Thread Content 2",
         comments:
         [
             {
                 author:"EcoUser",
                 date:Date.now(),
-                content:"Hey there"
+                content:"Eco park...."
 
             },
             {
                 author:"EcoUser",
                 date:Date.now(),
-                content:"Hey There"
+                content:"Eco park....."
             }
         ]
     }
